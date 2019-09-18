24750732d041389c24b87714c3c6ef419c31b386
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2019-09-18 12:01:59 -0300

    update game #1
    changed lot of stuff [array -> variables, function -> while, X -> space, dont clear screen -> clear screen]
    todo: HP, entities, objects

--- main.c
+++ main.c
@@ -1,57 +1,66 @@
 #include <stdio.h>
-#include <curses.h>
+#include <conio.h>
 
 #define U_16 0x20000            //U-16 text mode, for the text blocks
 
-void direction(char mov, int *player){
-    if (mov=='w')
-        player[1]++;
-    else if(mov=='s')
-        player[1]--;
-    else if(mov=='a')
-        player[0]--;
-    else if(mov=='d') 
-        player[0]++;
-}
-
 int main() {
-    char table[20][15];
+    char table[20][40];
     int x = 1;
-
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 40; j++) {
-            table[i][j] = '0';
-            printf("%c", table[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
     char mov;
 
-    while(x <= 2) {
-        int player[1];
-        player[0] = 0;
-        player[1] = 0;
+    int coordx = 1;
+    int coordy = 1;
 
-        table[player[0]][player[1]] = '0';
+    int asdasd = 1;
+    while(asdasd==1){
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 40; j++) {
+                table[i][j] = ' ';
+            }
+        }
 
-        printf("\n|_W_|\n"
-               "|ASD|\n");
-        mov = getch();
-        direction(mov,player);
+        table[player[1]][coordx] = 'X';
 
-        table[player[0]][player[1]] = 'X';
-
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 40; j++) {
                 printf("%c", table[i][j]);
             }
             printf("\n");
         }
-
         printf("\n");
-        x++;
+
+        printf("\ncoordx=%i\ncoordy=%i\n", coordx, coordy);
+
+        printf("\n|_W_|\n"
+               "|ASD|\n"
+               "[Q TO QUIT]\n");
+        mov = getch();
+
+        if (mov=='q')
+            asdasd = 0;
+        else if (mov=='w'){
+            if(coordy == 0)
+                printf("BONK");
+            else coordy--;
+        }
+        else if(mov=='s'){
+            if(coordy == 19)
+                printf("BONK");
+            else coordy++;
+        }
+        else if(mov=='a'){
+            if(coordx == 0)
+                printf("BONK");
+            else coordx--;
+        }
+        else if(mov=='d'){
+            if(coordx == 39)
+                printf("BONK");
+            else coordx++;
+        }
+
+
+        system("cls");
     }
 
     return 0;
