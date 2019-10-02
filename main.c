218f4f7a416e6fb6227a7be84f7b47b9cbb0784c
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2019-10-02 11:50:51 -0300

    Update #6
    cleaned the code AND integrated random seed generation for the boxes

--- main.c
+++ main.c
@@ -1,56 +1,45 @@
 #include <stdio.h>
 #include <conio.h>
+#include <time.h>
 
 #define U_16 0x20000  //U-16 text mode, for the text blocks -> ignorar isso, serve apenas caso tenha caractere especial
 
-int checkmovable(char dir, char table[20][40], int antesx, int antesy){ //portas lógicas para movimentação: para cima, para baixo, etc
-    int depoisx;
-    int depoisy;
-    int moveu = 0;
+typedef enum {CIMA, BAIXO, DIREITA, ESQUERDA} Dir;
 
+int checkmovable(Dir dir, char table[20][40], int antesx, int antesy){ //portas lógicas para movimentação: para cima, para baixo, etc
+    int depoisx = antesx;
+    int depoisy = antesy;
+    int moveu = 0;
     switch(dir){
-        case 'c':
-                        printf("cccccccccccccccc");
-            depoisx = antesx;
-            depoisy = antesy - 1;
-                        printf("cccccccccccccccc");
+        case CIMA:
+            depoisy--;
             if(table[depoisy][depoisx] == 'X' || table[depoisy][depoisx] == 'K'){ //|| quer dizer OU, se quiser dizer E, só colocar && (...) se o player tentar mecher o personagem para um bloco[K] ou parede[X], a função retorna negativo
                 return 0;
-                printf("cccccccccccccccc"); //nao consegue se mover
             }
             else moveu = 1;
-                        printf("cccccccccccccccc");
             break;
-        case 'e':
-                        printf("eeeeeeeeeeeeeeeee");
-            depoisx = antesx - 1;
-            depoisy = antesy;
+        case ESQUERDA:
+            depoisx--;
             if(table[depoisy][depoisx] == 'X' || table[depoisy][depoisx] == 'K')
                 return 0;
             else moveu = 1;
             break;
-        case 'b':
-                        printf("bbbbbbbbbbbbbbbbbb");
-            depoisx = antesx;
-            depoisy = antesy + 1;
+        case BAIXO:
+            depoisy++;
             if(table[depoisy][depoisx] == 'X' || table[depoisy][depoisx] == 'K')
                 return 0;
             else moveu = 1;
             break;
-        case 'd':
-                        printf("ddddddddddddd");
-            depoisx = antesx + 1;
-            depoisy = antesy;
+        case DIREITA:
+            depoisx++;
             if(table[depoisy][depoisx] == 'X' || table[depoisy][depoisx] == 'K')
                 return 0;
             else moveu = 1;
             break;
         default:
-                        printf("fffffffffffffff");
             printf("problema na recebicao do dir");
             break;
     }
-
     if (moveu == 1){
         table[antesy][antesx] = '_';
         table[depoisy][depoisx] = 'O';
@@ -80,6 +69,7 @@ int main() {
         }
     }
 
+    srand(time(NULL));
     for (int i = 0; i < 20; i++){
         for (int j = 0; j < 40; j++){
             if(table[i][j] == '_' && rand()%10 == 0) //aqui é gerado aleatoriamente vários blocos, que é K
@@ -92,9 +82,7 @@ int main() {
 
         if (hithead == 1) //caso não consiga se mecher = printa BONK
             printf("BONK");
-
         printf("\n");
-
         hithead = 0;
 
         table[coordy][coordx] = 'O'; //o player é renderizado em O
@@ -108,37 +96,36 @@ int main() {
         printf("\n");
 
         printf("\ncoordx = %i\ncoordy = %i\n", coordx, coordy); //printa as coords do player na tela
-
         printf("\n|_W_|\n"
                "|ASD|\n"
                "[Q TO QUIT]\n");
         mov = getch(); //pega o movimento do player do teclado q ele digitar, recebe a letra q tbm caso o player queira sair do jogo
 
-        char dir; //variável da direção, só usado para a função
+        Dir dir;
         switch(mov){
             case 'q':
                 varrepeat = 0; //acaba com o laço de repetição maior
                 break;
             case 'w':
-                dir = 'c';
+                dir = CIMA;
                 if(checkmovable(dir, table, coordx, coordy) == 0) //se não conseguir se mecher para [dir], variavel de não cosneguir se mecher é positiva
                     hithead = 1;
                 else coordy--; //caso consiga se mecher, a variavel da respectiva coordenada é alterada / o personagem se meche
                 break;
             case 's':
-                dir = 'b';
+                dir = BAIXO;
                 if(checkmovable(dir, table, coordx, coordy) == 0)
                     hithead = 1;
                 else coordy++;
                 break;
             case 'a':
-                dir = 'e';
+                dir = ESQUERDA;
                 if(checkmovable(dir, table, coordx, coordy) == 0)
                     hithead = 1;
                 else coordx--;
                 break;
             case 'd':
-                dir = 'd';
+                dir = DIREITA;
                 if(checkmovable(dir, table, coordx, coordy) == 0)
                     hithead = 1;
                 else coordx++;
@@ -146,6 +133,6 @@ int main() {
         }
     }
 
-    printf("\n[[[[JOGO FECHADO]]]]\n(pressione algo para fechar)"); //caso o player digite 'q' e saia do jogo
+    printf("\n[[[[JOGO FECHADO]]]]\n(pressione algo para fechar)"); //caso o player digite 'Q' e saia do jogo
     return 0; //fim
 }
